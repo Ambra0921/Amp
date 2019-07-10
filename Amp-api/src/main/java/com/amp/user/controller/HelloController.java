80507b4b810e67833f7c5f42a6f0241e079d654b
zhiyong.cai@ucarinc.com <zhiyong.cai@ucarinc.com>
2019-07-10 11:28:30 +0800

    项目调整+user类

--- Amp-api/src/main/java/com/amp/user/controller/HelloController.java
+++ Amp-api/src/main/java/com/amp/user/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.amp.controller;
+package com.amp.user.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ Amp-api/src/main/java/com/amp/user/model/UserInfo.java
@@ -0,0 +1,24 @@
+package com.amp.user.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class UserInfo implements Serializable {
+
+    private Long id;
+
+    private String userName;
+
+    private String password;
+
+    private Long creator;
+
+    private Long updator;
+
+    private Date createTime;
+
+    private Date updateTime;
+}
